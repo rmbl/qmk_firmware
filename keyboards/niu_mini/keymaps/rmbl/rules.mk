303a80dc7081bb1f012dbe37165102bc07bd0149
Philipp Gildein <philipp@gildein.com>
2018-01-10 01:03:41 +0100

    Add new keymap for niu_mini

---
+++ keyboards/niu_mini/keymaps/rmbl/rules.mk
@@ -0,0 +1,7 @@
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
+
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
