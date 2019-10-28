378413b7afccb8b1dec2d6bd54081a391b678f7e
Philipp Gildein <philipp@gildein.com>
2019-10-28 22:50:38 +0100

    Update ergotravel keymap

--- keyboards/ergotravel/keymaps/rmbl/config.h
+++ keyboards/ergotravel/keymaps/rmbl/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Use I2C or Serial, not both */
 
 #define USE_SERIAL
-//#define USE_I2C
+// #define USE_I2C
 
 /* Select hand configuration */
 
@@ -30,7 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 12
+#define RGBLED_NUM 14
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
